a055d707189a90d9544ed238db52509eeadc8a1f
심은주 <nyaknya@naver.com>
2024-05-23 00:01:08 +0900

    🐛 fix : 빌드 오류 해결을 위한 Link 삭제

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import ContentBox from "../../components/ContentBox";
 import "./style.css";
 import { ChangeEvent, FormEvent, useState, useEffect } from "react";
