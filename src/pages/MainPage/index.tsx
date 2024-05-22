bb97a08f83c8a5cbd3aedbacb9ad98d092301ff4
심은주 <nyaknya@naver.com>
2024-05-22 23:51:11 +0900

    ✨ feat: 주사위 계산 기능 추가
    - dice, slider 자리 변동될 수 있음.

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
 import ContentBox from "../../components/ContentBox";
 import "./style.css";
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import calculator from "../../utils/calculator";
 
 export default function Home() {
-  const [diceValue, setDiceValue] = useState<number>();
-  const [sliderValue, setSliderValue] = useState<number>();
+  const [diceValue, setDiceValue] = useState<number>(0);
+  const [sliderValue, setSliderValue] = useState<number>(0);
+  const [results, setResults] = useState<number[]>([]);
+
+  const handleDiceChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setDiceValue(Number(e.target.value));
+  };
+
+  const handleSliderChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSliderValue(Number(e.target.value));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const result = calculator(diceValue, sliderValue);
+    setResults(result);
+  };
 
   return (
     <div className="index-container">
@@ -13,20 +29,31 @@ export default function Home() {
       <ContentBox className="basic-calculator">
         <h3>일단 기본적인 확률 계산기를 만들어볼게요.</h3>
         <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="input-box">
-              <input type="text" placeholder="N" value={diceValue} />
+              <input
+                type="number"
+                value={diceValue}
+                onChange={handleDiceChange}
+                min="1"
+              />
               <span>d</span>
-              <input type="text" placeholder="N" value={sliderValue} />
+              <input
+                type="number"
+                value={sliderValue}
+                onChange={handleSliderChange}
+                min="1"
+              />
             </div>
-            <button className="basic-button">결과</button>
+            <button className="basic-button" type="submit">
+              결과
+            </button>
           </form>
         </div>
       </ContentBox>
       <ContentBox>
         <h4>결과창</h4>
-        <p>결과 : </p>
-        <p>판정 여부 :</p>
+        <p>결과 : {results.join(", ")}</p>
       </ContentBox>
     </div>
   );
--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -1,3 +1,7 @@
-export default function calculator(dice: number, slider: number) {
-  // dice d sider 이렇게 굴리는거임.
+export default function calculator(slider: number, dice: number): number[] {
+  const results = [];
+  for (let i = 0; i < slider; i++) {
+    results.push(Math.floor(Math.random() * dice) + 1);
+  }
+  return results;
 }
