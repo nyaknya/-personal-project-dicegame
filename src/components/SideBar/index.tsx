099d3059116bd68068e9ab9c98d8bdffed303081
심은주 <nyaknya@naver.com>
2024-06-07 00:56:47 +0900

    ✨ feat: 사이드바 토글 구현

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import "./style.css";
 import CharacterItem from "./CharacterItem";
 
 export default function SideBar() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="sidebar-wrap">
+    <div className={`sidebar-wrap ${isOpen ? "open" : "closed"}`}>
       <div className="character-list-header">
         <h2>캐릭터 리스트</h2>
-        <div className="close-button">
-          close <img src="/images/sidebar.svg" alt="사이드바 토글 아이콘" />
+        <div className="close-button" onClick={handleToggle}>
+          {isOpen ? "close" : "open"}{" "}
+          <img src="/images/sidebar.svg" alt="사이드바 토글 아이콘" />
         </div>
       </div>
-      <div className="character-list-container">
+      <div
+        className={`character-list-container ${isOpen ? "visible" : "hidden"}`}
+      >
         <div className="category">
           <span>이름</span>
           <span>체력</span>
@@ -21,35 +31,9 @@ export default function SideBar() {
           <span>상태</span>
         </div>
         <div className="character-list">
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
-          <CharacterItem />
+          {Array.from({ length: 30 }, (_, index) => (
+            <CharacterItem key={index} />
+          ))}
         </div>
       </div>
     </div>
