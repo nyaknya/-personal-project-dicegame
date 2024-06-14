68a5ec840ad3396526d2073b0a8aba4655368e2f
심은주 <nyaknya@naver.com>
2024-06-14 23:15:52 +0900

    💄 style: 트라이얼 타이틀 스타일링

--- src/components/Trial/index.tsx
+++ src/components/Trial/index.tsx
@@ -2,16 +2,16 @@ import "./style.css";
 
 export default function Trial() {
   return (
-    <>
+    <div className="trial-wrapper">
       <div className="trial-title">
         <h2>난관 굴림</h2>
-        <ul>
+        <ul className="trial-caption">
           <li>참여 인원 : N명</li>
           <li>호전성 총합 : NN</li>
           <li>창의성 총합 : NN</li>
           <li>이타성 총합 : NN</li>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
