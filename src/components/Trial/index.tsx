ad4b12589d44931f4c4060360177c778ce86f6fc
심은주 <nyaknya@naver.com>
2024-06-14 23:51:22 +0900

    ✨ feat: Zustand 설치 및 초기세팅

--- src/components/Trial/index.tsx
+++ src/components/Trial/index.tsx
@@ -5,6 +5,7 @@ export default function Trial() {
     <div className="trial-wrapper">
       <div className="trial-title">
         <h2>난관 굴림</h2>
+        <div className="trial-people-count">카운트 들어올곳(주스탠드 활용)</div>
         <ul className="trial-caption">
           <li>참여 인원 : N명</li>
           <li>호전성 총합 : NN</li>
