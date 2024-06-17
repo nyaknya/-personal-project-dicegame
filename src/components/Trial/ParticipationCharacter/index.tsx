9132d500de1992fe40b33253620460b8b73b1846
심은주 <nyaknya@naver.com>
2024-06-17 23:25:32 +0900

    ✨feat : 캐릭터의 이름 뿐 아닌 전체 정보를 가져오도록 수정

--- src/components/Trial/ParticipationCharacter/index.tsx
+++ src/components/Trial/ParticipationCharacter/index.tsx
@@ -2,17 +2,18 @@ import { useContext, useRef, useState } from "react";
 import "./style.css";
 import { CharactersContext } from "../../../context/CharactersContext";
 import useOutSideClick from "../../../hooks/useOutSideClick";
+import { Character } from "../../../types";
 
 export default function ParticipationCharacter() {
   const { characters } = useContext(CharactersContext);
-  const [selectedCharacter, setSelectedCharacter] = useState<string | null>(
+  const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(
     null
   );
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const ref = useRef<HTMLDivElement>(null);
 
-  const handleCharacterClick = (name: string) => {
-    setSelectedCharacter(name);
+  const handleCharacterClick = (character: Character) => {
+    setSelectedCharacter(character);
     setIsOpen(false);
   };
 
@@ -35,7 +36,7 @@ export default function ParticipationCharacter() {
           className={`now-selected ${selectedCharacter ? "" : "no-selection"}`}
           onClick={toggleOpenClose}
         >
-          {selectedCharacter ? selectedCharacter : "캐릭터를 선택하세요."}
+          {selectedCharacter ? selectedCharacter.name : "캐릭터를 선택하세요."}
           {isOpen ? (
             <img src="/images/up.svg" alt="위 화살표" />
           ) : (
@@ -48,7 +49,7 @@ export default function ParticipationCharacter() {
               <li
                 key={index}
                 className="character-option"
-                onClick={() => handleCharacterClick(character.name)}
+                onClick={() => handleCharacterClick(character)}
               >
                 {character.name}
               </li>
