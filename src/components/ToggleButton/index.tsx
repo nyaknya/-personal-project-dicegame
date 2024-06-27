77d98fcfc71dc79bb29b87c96bf06d8fc27686dd
심은주 <nyaknya@naver.com>
2024-06-27 18:27:10 +0900

    🔥 remove: 사이드바 토글 문구 삭제

--- src/components/ToggleButton/index.tsx
+++ src/components/ToggleButton/index.tsx
@@ -8,7 +8,6 @@ interface ToggleButtonProps {
 export default function ToggleButton({ isOpen, onToggle }: ToggleButtonProps) {
   return (
     <div className="close-button" onClick={onToggle}>
-      {isOpen ? "close" : "open"}
       <img src="/images/sidebar.svg" alt="토글 아이콘" />
     </div>
   );
