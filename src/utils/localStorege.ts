5e87b745c9a24ccdc385c09dc904076d6c6629c9
심은주 <nyaknya@naver.com>
2024-07-14 22:41:13 +0900

    ✏️ chore: 타입 지정

--- src/utils/localStorege.ts
+++ src/utils/localStorege.ts
@@ -1,10 +1,8 @@
-// 로컬 스토리지에 데이터 저장
-export const saveToLocalStorage = (key, value) => {
+export const saveToLocalStorage = (key: string, value: any): void => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
-// 로컬 스토리지에서 데이터 불러오기
-export const loadFromLocalStorage = (key, defaultValue) => {
+export const loadFromLocalStorage = <T>(key: string, defaultValue: T): T => {
   const savedData = localStorage.getItem(key);
   return savedData ? JSON.parse(savedData) : defaultValue;
 };
